135b0e344ba453ae26a282bd660e7ea7ef7eeddd
Håkan Wiman <78019492+HWiman-ICONIC@users.noreply.github.com>
2022-12-19 17:22:04 +0100

    Added a measure event - Enabled measuring and displaying points as an example - Renamed image canvas - Documented IconicMeasureHandler - Started on ImageToObject etc in handler

---
+++ include/IconicMeasureCommon/ImageCanvas.h
@@ -0,0 +1,185 @@
+#pragma once
+
+#include <IconicMeasureCommon/exports.h>
+#include <GL/glew.h>
+#include <wx/glcanvas.h>
+#include <boost/shared_ptr.hpp>
+#include <IconicVideo/ImageGLBase.h>
+#include <IconicGpu/IconicLog.h>
+
+namespace iconic {
+/**
+ * @brief Image display window.
+ * 
+ * Efficient display of images using e.g. GPU decoded image directly without copy to CPU. 
+ * Uses OpenGL to render images.
+*/
+class ICONIC_MEASURE_COMMON_EXPORT ImageCanvas : public wxGLCanvas, public ImageGLBase {
+public:
+	/**
+	 * @brief How to handle mouse events
+	*/
+	enum class EMouseMode {
+		MOVE,	//!< Move, pan, zoom in image
+		MEASURE	//!< Measure in image
+	};
+	/**
+	 * @brief Contructor
+	 * @param parent Parent window
+	 * @param canvasAttrs OpenGL attributes to be applied
+	 * @param nDispWidth Display width
+	 * @param nDispHeight Display height
+	 * @param nTexWidth Image width
+	 * @param nTexHeight Image height
+	 * @param bUsePbo Use Pixel Binary Object (PBO) or not
+	*/
+	ImageCanvas(wxWindow* parent, const wxGLAttributes& canvasAttrs, unsigned int nDispWidth,
+		unsigned int nDispHeight, unsigned int nTexWidth, unsigned int nTexHeight, bool bUsePbo);
+
+	/**
+	 * @brief Destructor
+	*/
+	virtual ~ImageCanvas();
+
+	/**
+	 * @brief Paints the image
+	 * @param event unused 
+	*/
+	virtual void OnPaint(wxPaintEvent& event);
+
+	/**
+	 * @brief Called when window is resized
+	 * @param event Size event
+	*/
+	virtual void OnSize(wxSizeEvent& event);
+
+	/**
+	 * @brief Refreshes window on idle
+	 * @param event unused
+	*/
+	virtual void OnIdle(wxIdleEvent& event);
+
+	/**
+	 * @brief Refreshes the image.
+	 * 
+	 * Overload from ImageGLBase. Only refreshes here, but can set up window and image sizes etc.
+	 * @param field_num unused
+	 * @param refresh Refresh
+	*/
+	virtual void render(int field_num, bool refresh);
+
+	/**
+	 * @brief Setup OpenGL.
+	 * 
+	 * No harm calling multiple times, since it will return directly if already initialized.
+	*/
+	virtual void initGL();
+
+	/**
+	 * @brief Set the associated OpenGl context as current
+	*/
+	virtual void SetCurrent();
+
+	/**
+	 * @brief Refresh the image window.
+	 * 
+	 * Overload from ImageGLBase.
+	*/
+	virtual void refresh();
+
+	/**
+	 * @brief Swap double buffers.
+	 * 
+	 * Calls OpenGL swap buffers
+	 * @return Always true
+	*/
+	virtual bool SwapBuffers();
+
+	/**
+	 * @brief Set image title
+	 * @param title Title
+	*/
+	virtual void SetTitle(char title[]);
+
+	/**
+	 * @brief Setup parameters for OpenGL.
+	 * 
+	 * Sets up OpenGL model and projection modes for the current window and image sizes.
+	*/
+	void ResetProjectionMode();
+
+	/**
+	 * @brief Fit image to window.
+	 * @param set Fit image to window.
+	*/
+	void SetFitToWindow(bool set = true);
+
+	/**
+	 * @brief Fit image to window
+	 * @return Fit image to window
+	*/
+	bool GetFitToWindow() const;
+
+	/**
+	 * @brief 
+	 * @param event 
+	*/
+	void OnMouse(wxMouseEvent& event);
+
+	/**
+	 * @brief 
+	 * @param event 
+	*/
+	void OnMouseWheel(wxMouseEvent& event);
+
+	/**
+	 * @brief Set mouse mode.
+	 *
+	 * Toggle between moving (and zooming) or measuring
+	 * @param mode Mouse mode
+	 * @todo Set mouse mode of the image window accordingly
+	*/
+	void SetMouseMode(EMouseMode mode);
+
+	/**
+	 * @brief Get mouse mode.
+	 * @return Mouse mode
+	 * @sa SetMouseMode
+	*/
+	EMouseMode GetMouseMode() const;
+
+	static boost::shared_ptr<wxGLContext> gpContext;
+
+	/**
+	 * @brief Transform screen coordinates to normalized camera coordinates.
+	 * @param pt Screen coordinates
+	 * @param x x camera coordinate [-0.5..+0.5]
+	 * @param y y camera coordinate [-0.5..+0.5]
+	*/
+	void ScreenToCamera(const wxPoint& pt, float& x, float& y);
+protected:
+	/**
+	 * @brief Handle mouse move.
+	 * 
+	 * Pan around and zoom
+	 * @param event Mouse position etc
+	*/
+	virtual void MouseMove(wxMouseEvent& event);
+
+	/**
+	 * @brief Handle measurement
+	 * @param event 
+	*/
+	virtual void MouseMeasure(wxMouseEvent& event);
+
+	void FitToWindow();
+	bool cbFitToWindow;
+	wxPoint cLastMousePos;
+	EMouseMode cMouseMode;
+	wxSize cLastClientSize;
+	GLdouble cOrthoWidth, cOrthoHeight;
+	std::vector<boost::compute::float2_> cvMeasurements;
+
+	wxDECLARE_EVENT_TABLE();
+};
+}
--- src/IconicMeasureCommon/IconicMeasureHandler.cpp
+++ src/IconicMeasureCommon/IconicMeasureHandler.cpp
@@ -1,18 +1,114 @@
 #include	<IconicMeasureCommon/IconicMeasureHandler.h>
+#include	<wx/filename.h>
+#include	<wx/log.h>
 
-IconicMeasureHandler::IconicMeasureHandler()
+IconicMeasureHandler::IconicMeasureHandler() :
+	cbIsParsed(false)
 {
 
 }
 
 bool IconicMeasureHandler::OnNextFrame(iconic::gpu::ImagePropertyPtr pProperties, wxString const& filename, int const& frameNumber, float const& time, boost::compute::uint2_ const& imSize, bool bDoParse) {
+	cpProperties = pProperties;
+	cImageFileName = filename;
+	wxFileName fn(cImageFileName);
+	fn.SetExt("dmap");
+	cDepthMapFileName = fn.GetFullPath();
+	fn.SetExt("cam");
+	cCameraFileName = fn.GetFullPath();
+	cbIsParsed = false;
+	if (bDoParse) {
+		if (!Parse()) {
+			wxLogError("Could not parse meta data");
+			return false;
+		}
+	}
 	return true;
 }
 
 wxString IconicMeasureHandler::GetMetaFileName() const {
-	return cMetaFileName;
+	return cDepthMapFileName;
 }
 
 bool IconicMeasureHandler::Parse() { 
-	return true; 
+	if (cbIsParsed) {
+		return true;
+	}
+	if (!wxFileName::FileExists(cDepthMapFileName)) {
+		wxLogError("Depth map is missing (%s)", cDepthMapFileName);
+		return false;
+	}
+	if (!wxFileName::FileExists(cCameraFileName)) {
+		wxLogError("Camera file is missing (%s)", cCameraFileName);
+		return false;
+	}
+	return true;
+}
+
+void IconicMeasureHandler::AddImagePolygon(PolygonPtr pPolygon, bool bAddObjectPolygon) {
+	if (bAddObjectPolygon) {
+		Polygon3DPtr pObject(new Polygon3D);
+		if (!ImageToObject(pPolygon, pObject)) {
+			wxLogError("Could not transform image polygon to object space");
+			return;
+		}
+		cvObjectPolygon.push_back(pObject);
+	}
+	cvImagePolygon.push_back(pPolygon);
+}
+
+bool IconicMeasureHandler::ImageToObject(PolygonPtr pImage, Polygon3DPtr pObject) {
+	if (!pImage || !pObject) {
+		wxLogError("No polygon");
+		return false;
+	}
+	size_t n = pImage->outer().size();
+	if (!n) {
+		wxLogError("No points in polygon");
+		return false;
+	}
+
+	if (pObject->outer().size() != n) {
+		pObject->outer().resize(n);
+	}
+	for (int i = 0; i < n; ++i) {
+		const Point& p = pImage->outer()[i];
+		Point3D& P = pObject->outer()[i];
+		if (!ImageToObject(p, P)) {
+			wxLogError("Could not transform image point to object");
+			return false;
+		}
+	}
+
+	return true;
+}
+
+bool IconicMeasureHandler::ImageToObject(const Point& pImage, Point3D& pObject) {
+	// ToDo Use camera and depth map to transform (col,row,Z) to (X,Y,Z) 
+	// Meanwhile just copy the image coordinates:
+	pObject.set<0>(pImage.get<0>());
+	pObject.set<1>(pImage.get<1>());
+	return true;
+}
+
+IconicMeasureHandler::PolygonPtr IconicMeasureHandler::CreatePolygon(size_t n) {
+	if (n == 0) {
+		return PolygonPtr(new Polygon);
+	}
+	else {
+		PolygonPtr p(new Polygon);
+		p->outer().resize(n);
+		return p;
+	}
+}
+
+IconicMeasureHandler::Polygon3DPtr IconicMeasureHandler::CreatePolygon3D(size_t n) {
+	if (n == 0) {
+		return Polygon3DPtr(new Polygon3D);
+	}
+	else {
+		Polygon3DPtr p(new Polygon3D);
+		p->outer().resize(n);
+		return p;
+	}
 }
