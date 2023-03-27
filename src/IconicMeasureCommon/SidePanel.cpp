76dc4c7f32897cf52779b53070f3ed66727426cc
Pontus <pontus.luthi@gmail.com>
2023-03-27 13:41:37 +0200

    sidepanel area now looks better

--- src/IconicMeasureCommon/SidePanel.cpp
+++ src/IconicMeasureCommon/SidePanel.cpp
@@ -4,15 +4,24 @@
 using namespace iconic;
 
 iconic::SidePanel::SidePanel(wxWindow* parent) : wxPanel(parent, wxID_ANY) {
-	area_text = new wxStaticText(this, wxID_ANY, wxString("Area"));
-	area_value = NULL;
+	wxPanel* area_panel = new wxPanel(this, wxID_ANY);
+	wxSizer* area_sizer = new wxBoxSizer(wxHORIZONTAL);
 
+	area_panel->SetBackgroundColour(wxColour(200, 200, 255));
 	sizer = new wxBoxSizer(wxVERTICAL);
+
+	area_text = new wxStaticText(area_panel, wxID_ANY, wxString("Area"), wxDefaultPosition, wxDefaultSize);
+	area_value = new wxStaticText(area_panel, wxID_ANY, wxString("placeholder"), wxDefaultPosition, wxDefaultSize);
+	
+	area_sizer->Add(area_text, 0, wxALIGN_CENTER_VERTICAL | wxALL , 10);
+	area_sizer->Add(area_value, 0, wxALIGN_CENTER_VERTICAL);
+
+	sizer->Add(area_panel, 0, wxEXPAND | wxALL | wxALIGN_CENTER_HORIZONTAL, 20);
+	
+	area_panel->SetSizerAndFit(area_sizer);
 	SetSizerAndFit(sizer);
-	sizer->Add(area_text, wxSizerFlags().Expand().Proportion(50));
-	sizer->Add(area_value, wxSizerFlags().Expand().Proportion(50));
 }
 
 void iconic::SidePanel::SetAreaText(wxString text) {
-	area_value = new wxStaticText(this, wxID_ANY, text);
+	area_value->SetLabel(text);
 }
